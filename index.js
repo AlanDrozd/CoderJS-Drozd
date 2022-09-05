8f5e91e1b42ffbb6eb4721631037e7c81f5e7913
Alan Drozd <alanezedrozd@gmail.com>
2022-09-05 19:48:47 -0300

    edit index.js

--- index.js
+++ index.js
@@ -25,32 +25,67 @@ if (edad >17) {
     alert("Por favor en caso de concretar una compra, consultalo antes con un adulto.")
 }
 
-alert ("Coderhourse 2022 - Entrega 2 - Alan Drozd")
+// function Productos(nombre, importe) {
+//     this.nombre = nombre;
+//     this.importe = importe;
+// }
 
-function productos(nombre, importe) {
-    this.nombre = nombre;
-    this.importe = importe;
+// const polerinYaro = new Productos('Polerin Yaro Negro', 3790);
+// const poleronDeanCrudo = new Productos('Poleron Dean Crudo', 8990);
+// const jeanJacob = new Productos('Jean Jacob', 8990);
+// const poleronDeanNegro = new Productos('Poleron Dean Negro', 8990);
+
+alert ("Coderhourse 2022 - Entrega 3 - Alan Drozd")
+
+const Stock = []
+
+class Product {
+    constructor (name, price){
+        this.name = name;
+        this.price = price;
+    }
+    showProduct(){
+        return this.name;
+    }
 }
 
-const polerinYaro = new productos('Polerin Yaro Negro', 3790);
-const poleronDeanCrudo = new productos('Poleron Dean Crudo', 8990);
-const jeanJacob = new productos('Jean Jacob', 8990);
-const poleronDeanNegro = new productos('Poleron Dean Negro', 8990);
+Stock.push (new Product('Polerin Yaro Negro', 3790));
+Stock.push (new Product('Poleron Dean Crudo', 8990));
+Stock.push (new Product('Jean Jacob', 7990));
+Stock.push (new Product('Poleron Dean Negro', 6990));
 
 let nombre = prompt("Hola! Antes de continuar, podrias decirnos tu nombre?");
 let saludo = alert (nombre + ", bienvenido a nuestra tienda!");
-let pedido = prompt("Cual de nuestros productos te interesa?" + "\n" + "1- " + polerinYaro.nombre + " $" + polerinYaro.importe + "\n" + "2- " + poleronDeanCrudo.nombre + " $" + poleronDeanCrudo.importe + "\n" + "3- " + jeanJacob.nombre + " $" + jeanJacob.importe + "\n" + "4- " + poleronDeanNegro.nombre + " $" + poleronDeanNegro.importe + "\n");
+let pedido = prompt("Cual de nuestros productos te interesa?" + "\n" + "1- " + Stock[0].name + " $" + Stock[0].price + "\n" + "2- " + Stock[1].name + " $" + Stock[1].price + "\n" + "3- " + Stock[2].name + " $" + Stock[2].price + "\n" + "4- " + Stock[3].name + " $" + Stock[3].price + "\n");
 let cantidad = prompt("Cuantos queres comprar?");
 
-if (pedido == 1) {
-    alert("Seleccionaste: " + polerinYaro.nombre + ", el total de tu pedido es de $" + polerinYaro.importe * cantidad);
-    } else if (pedido == 2) {
-    alert("Seleccionaste: " + poleronDeanCrudo.nombre + ", el total de tu pedido es de $" + poleronDeanCrudo.importe * cantidad);
-    } else if (pedido == 3) {
-    alert("Seleccionaste: " + jeanJacob.nombre + ", el total de tu pedido es de $" + jeanJacob.importe * cantidad);
-    } else if (pedido == 4) {    
-    alert("Seleccionaste: " + poleronDeanNegro.nombre + ", el total de tu pedido es de $" + poleronDeanNegro.importe * cantidad);
-    } else {
-    alert("No contamos con ese producto");
-    }       
-    alert("Gracias por elegirnos. Seguinos en redes sociales para encontrar descuentos!")
+switch (pedido) {
+    case pedido:
+        alert("Seleccionaste: " + Stock[pedido-1].name + ", el total de tu pedido es de $" + Stock[pedido-1].price * cantidad);
+    break;
+    // case "2":
+    //     alert("Seleccionaste: " + Stock[pedido-1].name + ", el total de tu pedido es de $" + Stock[pedido-1].price * cantidad);
+    // break;
+    // case "3":
+    //     alert("Seleccionaste: " + Stock[pedido-1].name + ", el total de tu pedido es de $" + Stock[pedido-1].price * cantidad);
+    // break;
+    // case "4":
+    //     alert("Seleccionaste: " + Stock[3].name + ", el total de tu pedido es de $" + Stock[3].price * cantidad);
+    // break;
+    default:
+        alert("No contamos con ese producto");
+}
+
+// if (pedido == 1) {
+//     alert("Seleccionaste: " + polerinYaro.nombre + ", el total de tu pedido es de $" + polerinYaro.importe * cantidad);
+//     } else if (pedido == 2) {
+//     alert("Seleccionaste: " + poleronDeanCrudo.nombre + ", el total de tu pedido es de $" + poleronDeanCrudo.importe * cantidad);
+//     } else if (pedido == 3) {
+//     alert("Seleccionaste: " + jeanJacob.nombre + ", el total de tu pedido es de $" + jeanJacob.importe * cantidad);
+//     } else if (pedido == 4) {    
+//     alert("Seleccionaste: " + poleronDeanNegro.nombre + ", el total de tu pedido es de $" + poleronDeanNegro.importe * cantidad);
+//     } else {
+//     alert("No contamos con ese producto");
+//     }       
+
+alert("Gracias por elegirnos. Seguinos en redes sociales para encontrar descuentos!")
