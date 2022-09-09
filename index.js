c4b18f0e87a6fd60f6d417bb7aa136d519b056d9
Alan Drozd <alanezedrozd@gmail.com>
2022-09-08 22:26:04 -0300

    edit index.js

--- index.js
+++ index.js
@@ -25,67 +25,43 @@ if (edad >17) {
     alert("Por favor en caso de concretar una compra, consultalo antes con un adulto.")
 }
 
-// function Productos(nombre, importe) {
-//     this.nombre = nombre;
-//     this.importe = importe;
-// }
-
-// const polerinYaro = new Productos('Polerin Yaro Negro', 3790);
-// const poleronDeanCrudo = new Productos('Poleron Dean Crudo', 8990);
-// const jeanJacob = new Productos('Jean Jacob', 8990);
-// const poleronDeanNegro = new Productos('Poleron Dean Negro', 8990);
-
-alert ("Coderhourse 2022 - Entrega 3 - Alan Drozd")
-
-const Stock = []
+const stock = []
 
 class Product {
-    constructor (name, price){
+    constructor (id, name, price, category){
+        this.id = id;
         this.name = name;
         this.price = price;
+        this.category = category;        
     }
     showProduct(){
         return this.name;
     }
 }
 
-Stock.push (new Product('Polerin Yaro Negro', 3790));
-Stock.push (new Product('Poleron Dean Crudo', 8990));
-Stock.push (new Product('Jean Jacob', 7990));
-Stock.push (new Product('Poleron Dean Negro', 6990));
+stock.push (new Product(1, 'Polerin Yaro Negro', 3790, 'Ropa'));
+stock.push (new Product(2, 'Poleron Dean Crudo', 8990, 'Ropa'));
+stock.push (new Product(3, 'Jean Jacob', 5490, 'Ropa'));
+stock.push (new Product(4, 'Poleron Dean Negro', 6990, 'Ropa'));
+stock.push (new Product(5, 'Kit Male Grooming', 5890, 'MakeUp'))
+stock.push (new Product(6, 'Mascara en gel', 1520, 'MakeUp'))
+stock.push (new Product(7, 'Primer Filler', 1290, 'MakeUp'))
+stock.push (new Product(8, 'Corrector Liquido', 1290, 'MakeUp'))
 
 let nombre = prompt("Hola! Antes de continuar, podrias decirnos tu nombre?");
 let saludo = alert (nombre + ", bienvenido a nuestra tienda!");
-let pedido = prompt("Cual de nuestros productos te interesa?" + "\n" + "1- " + Stock[0].name + " $" + Stock[0].price + "\n" + "2- " + Stock[1].name + " $" + Stock[1].price + "\n" + "3- " + Stock[2].name + " $" + Stock[2].price + "\n" + "4- " + Stock[3].name + " $" + Stock[3].price + "\n");
-let cantidad = prompt("Cuantos queres comprar?");
+let sector = prompt("Por favor, selecciona una categoria:" + "\n" + "1- " + stock[0].category + "\n" + "2- " + stock[4].category) 
 
-switch (pedido) {
-    case pedido:
-        alert("Seleccionaste: " + Stock[pedido-1].name + ", el total de tu pedido es de $" + Stock[pedido-1].price * cantidad);
-    break;
-    // case "2":
-    //     alert("Seleccionaste: " + Stock[pedido-1].name + ", el total de tu pedido es de $" + Stock[pedido-1].price * cantidad);
-    // break;
-    // case "3":
-    //     alert("Seleccionaste: " + Stock[pedido-1].name + ", el total de tu pedido es de $" + Stock[pedido-1].price * cantidad);
-    // break;
-    // case "4":
-    //     alert("Seleccionaste: " + Stock[3].name + ", el total de tu pedido es de $" + Stock[3].price * cantidad);
-    // break;
-    default:
-        alert("No contamos con ese producto");
+let ropa =0;
+
+if (sector == 1){
+    ropa = parseInt(prompt("Cual de nuestros productos te interesa?" + "\n" + "1- " + stock[0].name + " $" + stock[0].price + "\n" + "2- " + stock[1].name + " $" + stock[1].price + "\n" + "3- " + stock[2].name + " $" + stock[2].price + "\n" + "4- " + stock[3].name + " $" + stock[3].price + "\n"))+1
+    }   else{
+    ropa = parseInt(prompt("Cual de nuestros productos te interesa?" + "\n" + "1- " + stock[4].name + " $" + stock[4].price + "\n" + "2- " + stock[5].name + " $" + stock[5].price + "\n" + "3- " + stock[6].name + " $" + stock[6].price + "\n" + "4- " + stock[7].name + " $" + stock[7].price + "\n"))+3
 }
 
-// if (pedido == 1) {
-//     alert("Seleccionaste: " + polerinYaro.nombre + ", el total de tu pedido es de $" + polerinYaro.importe * cantidad);
-//     } else if (pedido == 2) {
-//     alert("Seleccionaste: " + poleronDeanCrudo.nombre + ", el total de tu pedido es de $" + poleronDeanCrudo.importe * cantidad);
-//     } else if (pedido == 3) {
-//     alert("Seleccionaste: " + jeanJacob.nombre + ", el total de tu pedido es de $" + jeanJacob.importe * cantidad);
-//     } else if (pedido == 4) {    
-//     alert("Seleccionaste: " + poleronDeanNegro.nombre + ", el total de tu pedido es de $" + poleronDeanNegro.importe * cantidad);
-//     } else {
-//     alert("No contamos con ese producto");
-//     }       
+let cantidad = Number(prompt("Elegiste " + stock[ropa].name + ". Cuantos queres comprar?"));
+
+alert("Seleccionaste " + cantidad +" unidades del producto: " + stock[ropa].name + " por un total de: $" +(cantidad * stock[ropa].price ))
 
 alert("Gracias por elegirnos. Seguinos en redes sociales para encontrar descuentos!")
